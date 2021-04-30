299d0bfd6f95c7da3a593694c8bd7c11210c1f5f
Connie Ho <hconnie09@gmail.com>
2021-04-29 21:18:05 -0600

    refactor text input example

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import TextExample from './TextExample';
 
 export default function App() {
 
-  const [name, setName] = useState('Connie');
-  const [age, setAge] = useState(25);
-
-
-
-
   return (
     <View style={styles.container}>
-      <Text>Enter Name:</Text>
-      <TextInput
-        multiline 
-        style={styles.input}
-        placeholder='e.g. John Doe'
-        onChangeText={(val)=> setName(val)}
-        />
-      <Text>Enter Age:</Text>
-      <TextInput 
-        keyboardType='numeric'
-        style={styles.input}
-        placeholder='e.g. 99'
-        onChangeText={(val)=> setAge(val)}
-        />
-      <Text>name: {name}, age: {age}</Text>
+      <TextExample />
     </View>
   );
 }
@@ -37,11 +18,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
+
 });
