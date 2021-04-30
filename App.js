e1509f5a9fc17e4e0a4cd45e6966027156eb65a8
Connie Ho <hconnie09@gmail.com>
2021-04-29 21:36:28 -0600

    refactor list to flatlist

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React, {useState} from 'react';
-import { StyleSheet, View} from 'react-native';
-import TextExample from './TextExample';
+import { StyleSheet, View, Text, FlatList} from 'react-native';
 
 export default function App() {
 
+  const [people, setPeople] = useState([
+    { name: 'connie', key: '1'},
+    { name: 'yoshi', key: '2'},
+    { name: 'mario', key: '3'},
+    { name: 'luigi', key: '4'},
+    { name: 'peach', key: '5'},
+    { name: 'toad', key: '6'},
+    { name: 'bowser', key: '7'},
+  ])
+
+
   return (
     <View style={styles.container}>
-      <TextExample />
+      <FlatList 
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.person}>{item.name}</Text>
+        )}
+      />
+      {/* <ScrollView>
+      { people.map(person => (
+            <View key={person.key} >
+              <Text style={styles.person}>{person.name}</Text>
+            </View>
+      ))}
+      </ScrollView> */}
     </View>
   );
 }
@@ -15,8 +37,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
   },
+  person: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
+  }
 
 });
