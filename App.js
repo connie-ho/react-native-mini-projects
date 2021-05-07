b13c8596b012359ff6a95d7a3d6d5e1e14c9004f
Connie Ho <hconnie09@gmail.com>
2021-05-07 12:01:23 -0600

    convert objects keys to string

--- App.js
+++ App.js
@@ -2,20 +2,35 @@ import React, {useState} from 'react';
 import { StyleSheet, View, Text, FlatList} from 'react-native';
 import Header from './components/todo/Header';
 import TodoItem from './components/todo/TodoItem';
+import Form from './components/todo/Form';
 
 export default function App() {
 
   const [todos, setTodos] = useState([
-    {text: 'buy coffee', key: 1},
-    {text: 'create an app', key: 2},
-    {text: 'apply to jobs', key:  3},
-    {text: 'do a leetcode practice problem', key: 4}
+    {text: 'buy coffee', key: '1', created_at: Date.now()},
+    {text: 'create an app', key: '2', created_at: Date.now()},
+    {text: 'apply to jobs', key:  '3', created_at: Date.now()},
+    {text: 'do a leetcode practice problem', key: '4', created_at: Date.now()}
   ])
 
   const pressHandler = (key) => {
     setTodos((prev) => {
-      return prev.filter(todo => todo.key != key)
+      return prev.filter(todo => todo.key != key);
     })
+  }
+
+  const submitHandler = (text, setText) => {
+    setTodos((prev) => {
+      return [
+        ...prev,
+        { text, 
+          key: Math.random().toString(), 
+          created_at: Date.now()
+        }
+      ]
+    })
+
+    setText(prev => '');
   }
 
 
@@ -23,10 +38,12 @@ export default function App() {
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* {to do form} */}
+        <Form 
+          submitHandler={submitHandler}
+        />
         <View style={styles.list}>
           <FlatList
-            data={todos}
+            data={todos.sort((a, b) => b.created_at - a.created_at)}
             renderItem={({item}) => (
               <TodoItem 
                 item={item} 
---
+++ README.md
@@ -0,0 +1,3 @@
+## A collection of mini React-Native projects
+
+# Todo List
